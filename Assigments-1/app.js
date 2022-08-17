ba09ff75741ce9384b9e6dd8784f307ffffcd0b5
adem kalfalı <ademkalfa9@gmail.com>
2022-08-17 07:33:12 +0300

    Week Days

--- Assigments-1/app.js
+++ Assigments-1/app.js
@@ -1,16 +1,43 @@
 console.log("*****ASSİGMENT-1*****")
 
-const n1 = +prompt("sayi1:");
-const n2 = +prompt("sayi2:");
-const islem = prompt("is:");
-if( islem == "+" ){
-    console.log(`${n1}+${n2}:${n1 + n2}`);
-}else if( islem == "-" ){
-    console.log(`${n1}-${n2} : ${n1 - n2}`);
-}else if( islem == "*"){
-    console.log(`${n1}*${n2} : ${n1 * n2}`);
-}else if ( islem == "/"){
-    console.log(`${n1}/${n2} : ${n1 / n2}`);
-}else{
-    console.log(`"Please enter operator"`);
+//Basic Calculator
+
+// const n1 = +prompt("sayi1:");
+// const n2 = +prompt("sayi2:");
+// const islem = prompt("is:");
+// if( islem == "+" ){
+//     console.log(`${n1}+${n2}:${n1 + n2}`);
+// }else if( islem == "-" ){
+//     console.log(`${n1}-${n2} : ${n1 - n2}`);
+// }else if( islem == "*"){
+//     console.log(`${n1}*${n2} : ${n1 * n2}`);
+// }else if ( islem == "/"){
+//     console.log(`${n1}/${n2} : ${n1 / n2}`);
+// }else{
+//     console.log(`"Please enter operator"`);
+// }
+
+
+//Week Days
+
+let day = +prompt("Enter days between 1-7 ")
+
+switch(day){
+    case 1: alert(`Today is Monday`);
+        break;
+    case 2: alert(`Today is Tuesday`);
+        break;
+    case 3: alert(`Today is Wednesday`);
+        break;
+    case 4: alert(`Today is Thursday`);
+        break;
+    case 5: alert(`Today is Friday`);
+        break;
+    case 6: alert(`Today is Saturday`);
+        break;
+    case 7: alert(`Today is Sunday`);
+    default:
+        alert("Should be between day 1-7")
+        break;
 }
+// console.log(outday);
