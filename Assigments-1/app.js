ca5dc3e1f01d288cdc3d3422b5de00b4ebffbc4f
adem kalfalı <ademkalfa9@gmail.com>
2022-08-20 08:03:23 +0300

    Grade average

--- Assigments-1/app.js
+++ Assigments-1/app.js
@@ -1,4 +1,4 @@
-console.log("*****ASSİGMENT-1*****")
+console.log("*****ASSİGMENT-1*****");
 
 //Basic Calculator
 
@@ -16,7 +16,6 @@ console.log("*****ASSİGMENT-1*****")
 // }else{
 //     console.log(`"Please enter operator"`);
 // }
-
 
 //Week Days
 
@@ -60,7 +59,34 @@ console.log("*****ASSİGMENT-1*****")
 
 //credit risk program
 
-const income  = +prompt("Enter İncome:");
-const expense = +prompt("Enter Expense:");
+// const income  = +prompt("Enter İncome:");
+// const expense = +prompt("Enter Expense:");
 
-`${income-expense}` >= 5500 && `${income-expense}` <= 5500 ? console.log("Kredi Verilir") : console.log("Kredi Verilmez");
+// `${income-expense}` >= 5500 && `${income-expense}` <= 5500 ? console.log("Kredi Verilir") : console.log("Kredi Verilmez");
+
+//Grade average
+
+let total = 0;
+let piece = 0;
+let again = true;
+
+do {
+  const n = prompt("Enter number or Q or q");
+  if (isNaN(n)) {
+    if (n === "q" || n === "Q") {
+      again = false;
+      break;
+    } else {
+      alert("Please enter a Number  or Q or q");
+    }
+  } else {
+    total += +n;
+    piece += 1;
+    console.log(total, n);
+  }
+} while (again);
+if (piece > 0) {
+  console.log(`Grade average:`, total / piece);
+} else {
+  console.log("Not enter number, have a nice day");
+}
